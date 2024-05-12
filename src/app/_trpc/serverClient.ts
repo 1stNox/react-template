45b172bdfaa75bb75118e0d7536ff5edf905fc4e
Finn-Lasse Reichling <reichlingfin@Finn-Lasses-MBP.fritz.box>
2024-05-12 16:46:31 +0200

    removed unused import

--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -1,5 +1,4 @@
 import { appRouter } from "@/server";
-import { httpBatchLink } from "@trpc/client";
 import { createCallerFactory } from "@trpc/server";
 
 const createCaller = createCallerFactory()
